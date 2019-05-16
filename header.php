4bd2cbbbe87ac14a99fe35a600291ba4a974080f
The CSMS <31397961+thecsmsdev@users.noreply.github.com>
2019-05-16 09:35:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html class="no-js">
+<html class="no-js" lang="en">
 <head>
   <title><?php wp_title('•', true, 'right'); bloginfo('name'); ?></title>
   <meta charset="utf-8">
@@ -7,7 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
-  <link rel="shortcut icon" href="" />
+  <!-- Favicons -->
+    <link rel="shortcut icon" href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/favicon.ico" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/apple-touch-icon.png" rel="apple-touch-icon" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/apple-touch-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/apple-touch-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/icon-hires.png" rel="icon" sizes="192x192" />
+    <link href="/wp-content/themes/INSERT-THEME-NAME-HERE/theme/images/favicons/icon-normal.png" rel="icon" sizes="128x128" /> 
 
 
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
@@ -18,54 +26,29 @@
 <body <?php body_class(); ?>>
 
 
-<div class="main-header">
-  <div class="header container">
-    <div class="logo-container">
-      <a href="<?php echo home_url('/'); ?>"><img alt="Logo" src="<?php bloginfo('template_directory'); ?>/theme/img/logo.png"></a>
-    </div>
-  </div>
-  <div class="row nav-maincontainer">
+<div class="container">
+
+	<div class="row">
     
-      <div class="col-lg-12">
+    	<div class="col-lg-12">
     
-            <nav class="navbar navbar-topbar">
-              <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-                &#9776;
-              </button>
-              <div class="collapse navbar-toggleable-xs" id="collapsingNavbar">
-                
-              <?php
-                  wp_nav_menu( array(
-                      'menu'              => 'mainmenu',
-                      'theme_location'    => 'navbar',
-                      'menu_class'        => 'nav navbar-nav', 
-                      'echo'              => true,
-                      'items_wrap'        => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                      'depth'             => 10,
-                  ) ); 
-              ?>
-              </div>
+            <!-- Menu Container -->
+            <nav class="header-nav">
+                <?php
+                    wp_nav_menu( array(
+                        'theme_location' => 'navbar',
+                        'menu_class' => 'nav navbar-nav', 
+                        'echo' => true,
+                        'fallback_cb' => '__return_false',
+                        'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                        'depth' => 10, 
+                        'walker' => new themeslug_walker_nav_menu
+                    ) );
+                ?>
             </nav>
         
         </div>
 
-  </div>
-</div>
-  
-<?php /*
-Site Title
-==========
-If you are displaying your site title in the "brand" link in the Bootstrap navbar, 
-then you probably don't require a site title. Alternatively you can use the example below. 
-See also the accompanying CSS example in theme/css/b4st.css .
+	</div>
 
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12">
-      <h1 id="site-title">
-        <a class="text-muted" href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-      </h1>
-    </div>
-  </div>
 </div>
-*/ ?>
